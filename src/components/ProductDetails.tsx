4dfcbf110b916720b3636be2ef75114a508b69f9
sebasmmf <sebastian.mart.f@gmail.com>
2024-08-09 18:47:06 -0500

    first commit

---
+++ src/components/ProductDetails.tsx
@@ -0,0 +1,71 @@
+import { useNavigate, Form, ActionFunctionArgs, redirect, useFetcher } from "react-router-dom"  
+import { Product } from "../types"
+import { formatCurrency } from "../utils"
+import { deleteProduct } from "../services/ProductService"
+
+type ProductDetailsProps = {
+    product: Product
+}
+
+export async function action({params} : ActionFunctionArgs) {
+    if (params.id !== undefined) {
+        await deleteProduct(+params.id)
+        return redirect('/')
+    }
+}
+
+export default function ProductDetails({product} : ProductDetailsProps) {
+
+    const fetcher = useFetcher()
+    const navigate = useNavigate()
+    const isAvailable = product.availability
+
+    return (
+        <tr className="border-b hover:bg-slate-200 hover:scale-105 transition">
+            <td className="p-3 text-lg text-gray-800 text-left">
+                {product.name}
+            </td>
+            <td className="p-3 text-lg text-gray-800 text-center">
+                {formatCurrency(product.price)}
+            </td>
+            <td className="p-3 text-lg text-gray-800 text-center">
+                <fetcher.Form method="POST">
+                    <button
+                        type="submit"
+                        name="id"
+                        value={product.id}
+                        className={`${isAvailable ? 'text-black' : 'text-red-600'} rounded-lg p-2 text-sm uppercase font-bold w-full border hover:border-black hover:cursor-pointer transition`}
+                    >
+                        {isAvailable ? 'Disponible' : 'No Disponible'}
+                    </button>
+                </fetcher.Form>
+            </td>
+            <td className="p-3 text-lg text-gray-800">
+                <div className="flex gap-2 items-center">
+                    <button
+                        onClick={() => navigate(`/productos/${product.id}/editar`)}
+                        className="bg-indigo-600 text-white rounded-lg w-full p-2 uppercase font-bold text-xs text-center hover:bg-indigo-700 transition"
+                    >
+                        Editar
+                    </button>
+                    <Form
+                        className="w-full"
+                        method="POST"
+                        action={`productos/${product.id}/eliminar`}
+                        onSubmit={(e) => {
+                            if (!confirm('¿Eliminar?')) {
+                                e.preventDefault()
+                            }
+                        }}
+                    >
+                        <input
+                            type="submit"
+                            value='Eliminar'
+                            className="bg-red-600 text-white rounded-lg w-full p-2 uppercase font-bold text-xs text-center hover:bg-red-700 transition"
+                        />
+                    </Form>
+                </div>
+            </td>
+        </tr> 
+    )
+}
